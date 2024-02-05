e59c2f479e2f690fca99eb9c31f04ff8de1a2dda
Darien Lin <darienlin87@gmail.com>
2024-02-05 15:43:50 -0800

    added pics

--- scripts.js
+++ scripts.js
@@ -7,11 +7,16 @@ var img = document.createElement("img");
   img.style.zIndex = 5;
   
   img.style.position = 'absolute';
-  img.style.top = document.body.clientHeight * Math.random() + 'px';
+  img.style.top = document.body.clientHeight * 3/4 + document.body.clientHeight/2  * Math.random() + 'px';
   img.style.left = document.body.clientWidth * Math.random() + 'px';
   document.body.appendChild(img);
 }
 
 document.getElementById('no').addEventListener('click', () =>
-  randomImage("http://placekitten.com/200/300", 200, 300, 'no')
+  randomImage(getRandomImageLink(), 150, 200, 'no')
 );
+
+function getRandomImageLink(){
+    const pics = ["images/cutie.png", "images/erm.jpeg",'images/frat.jpeg','images/hello.jpeg','images/licky.jpeg','images/man.jpeg','images/scp.jpeg','images/scp2.jpeg']
+    return pics[Math.floor(Math.random() * pics.length)]
+}
